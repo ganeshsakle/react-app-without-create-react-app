27ed8e75aceaaa47de3ea63ab88ca51bcd26545b
ganeshsakle <ganeshsakle7@gmail.com>
2023-08-20 17:47:58 +0530

    packages, npm and js added

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1", {}, "Hello World!");
+
+const heading2 = React.createElement("h2", {}, "This is a react app");
+
+const headings = React.createElement("div", {}, [heading, heading2]);
+
+const div1 = document.getElementById("div1");
+
+const root = ReactDOM.createRoot(div1);
+
+root.render(headings);
